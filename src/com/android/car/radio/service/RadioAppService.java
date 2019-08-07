1bfcc0bd3e5c275a7e64384da097ed0a7c7c4b0f
Tomasz Wasilczyk <twasilczyk@google.com>
2019-08-07 13:29:55 -0700

    Don't crash trying to restore invalid program
    Bug: 135443669
    Test: mock invalid ProgramInfo, build, run
    Change-Id: Ia434cfc6183c06b0a33fa9b305d81a1eb816e8e7

--- src/com/android/car/radio/service/RadioAppService.java
+++ src/com/android/car/radio/service/RadioAppService.java
@@ -214,7 +214,11 @@ public class RadioAppService extends MediaBrowserService implements LifecycleOwn
             ProgramSelector sel = mRadioStorage.getRecentlySelected(pt);
             if (sel != null) {
                 Log.i(TAG, "Restoring recently selected program: " + sel);
-                mRadioTuner.tune(sel, tuneCb);
+                try {
+                    mRadioTuner.tune(sel, tuneCb);
+                } catch (IllegalArgumentException | UnsupportedOperationException e) {
+                    Log.e(TAG, "Can't restore recently selected program: " + sel, e);
+                }
                 return;
             }
 
